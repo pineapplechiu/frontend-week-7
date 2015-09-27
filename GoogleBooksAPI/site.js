ea2483dbe913684c61869768f684f6a03b86aac6
Paula Chiu <chiupaula@gmail.com>
2015-09-26 22:50:29 -0700

    continue exercise

--- GoogleBooksAPI/site.js
+++ GoogleBooksAPI/site.js
@@ -1,24 +1,38 @@
 $(function() {
-  
   var i = 0;
   var searchTerm = '';
-  
-  function getBooks(subject, startIndex){
+  function getBooks(subject, startIndex) {
     $.get('https://www.googleapis.com/books/v1/volumes', {
       q: subject,
       startIndex: startIndex,
       maxResults: 20
     }, 
-  function(data) {
-    //display all 20 results on page
-    });   
-  };
-
-  function search(){
+    function(data) {
+      console.log(data); // where in the array?
+      i = data.items.length;
+      $.each(data.items, function(index, value) {
+        var div = $('<div>');
+        div.appendTo('#results');
+        // data is stored in volumnInfo
+        var title = value.volumeInfo.title;
+        var author = value.volumeInfo.authors;
+        var pubDate = value.volumeInfo.publishedDate;
+        div.html(title + '</br>' + 'by ' + author + '</br>' + pubDate + '</br>');
+        var img = $('<img>');
+        img.attr('src', value.volumeInfo.imageLinks.thumbnail);
+        img.appendTo(div);
+      });
+    });
+  }
+  function search() {
     searchTerm = $('.searchBox').val();
     getBooks(searchTerm, i); 
   }
-
-  $('.searchBtn').click(search);
   
-});
+  $('.searchBtn').click(search);
+
+  $('#my_button').click(
+  function() {
+    getBooks(searchTerm, i);
+  })
+})  
