06722d7f02c2a93e4f1a56eec6f19e9fa418de5f
Paula Chiu <chiupaula@gmail.com>
2015-09-26 21:07:51 -0700

    partial answer for book search exercise

--- GoogleBooksAPI/site.js
+++ GoogleBooksAPI/site.js
@@ -1,27 +1,24 @@
+$(function() {
+  
+  var i = 0;
+  var searchTerm = '';
+  
+  function getBooks(subject, startIndex){
+    $.get('https://www.googleapis.com/books/v1/volumes', {
+      q: subject,
+      startIndex: startIndex,
+      maxResults: 20
+    }, 
+  function(data) {
+    //display all 20 results on page
+    });   
+  };
 
+  function search(){
+    searchTerm = $('.searchBox').val();
+    getBooks(searchTerm, i); 
+  }
 
-var maxResults = 20;
-var startIndex = 0;
-
-$(document).ready(function(){
-    var searchterm; 
-
-    $("#button").click(function(){
-        searchterm = $("#term").val();
-        find();
-    });
-
-    function find(){
-    $.get("https://www.googleapis.com/books/v1/volumes?q=" + searchterm)
-       .done(function(data){
-       $("#results").text(data.items[0].volumeInfo.authors[0]);
-       });
-
-      //   console.log(find.text);
-      // });
-   }
-
-   //  $.get("https://www.googleapis.com/books/v1/volumes", {q: searchterm, startIndex: start, maxResults: num-of-results}, function(data){ do something here });
-
-    
+  $('.searchBtn').click(search);
+  
 });
